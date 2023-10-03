16ce14c5a792317c1bb29b542ab1cd9718e44f8e
Francisca Rosa <f.rosa96@hotmail.com>
2023-10-02 21:23:54 -0300

    exercicio5 aula 4

---
+++ Aula4/Exercicios/exercicio5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char str[], int inicio, int fim) {
+    // Caso base: se a string tiver 0 ou 1 caracteres, é um palíndromo
+    if (inicio >= fim) {
+        return 1;
+    }
+    
+    // Verifique se os caracteres nas posições 'inicio' e 'fim' são iguais
+    if (str[inicio] != str[fim]) {
+        return 0; 
+    }
+    
+    // Chamada recursiva para verificar os caracteres internos
+    return isPalindrome(str, inicio + 1, fim - 1);
+}
+
+int main() {
+    char str[100];
+    
+    scanf("%s", str);
+    
+    int len = strlen(str);
+    int result = isPalindrome(str, 0, len - 1);
+    
+    if (result) {
+        printf("Eh um palindromo.\n");
+    } else {
+        printf("Não eh um palindromo.\n");
+    }
+    
+    return 0;
+}
+
+
+
+
+
